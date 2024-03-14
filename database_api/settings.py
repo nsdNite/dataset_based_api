6e9ce65e322aeb3c1f28ac1cd2c56ec80da963b3
nsdNite <nsd7night@gmail.com>
2024-03-14 12:37:22 +0200

    Add swagger support

--- database_api/settings.py
+++ database_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "API",
 ]
 
@@ -125,3 +126,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Client database API",
+    "DESCRIPTION": "API for marketplace clients.",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
--- database_api/urls.py
+++ database_api/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("API.urls", namespace="clients")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
