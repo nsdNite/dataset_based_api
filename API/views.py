9c59cb46f17dc5a046c7c45dfda5a28b3c59b333
nsdNite <nsd7night@gmail.com>
2024-03-14 10:56:30 +0200

    Add pagination to client view

--- API/views.py
+++ API/views.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 
 from API.models import Client
 from API.serializers import ClientSerializer
+from API.pagination import ClientPagination
 
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    pagination_class = ClientPagination
