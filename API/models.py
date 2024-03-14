f8a1a0f2eda044b88b7f21a0e6ecc19990b3f4bf
nsdNite <nsd7night@gmail.com>
2024-03-14 09:54:27 +0200

    add models

--- API/models.py
+++ API/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+
+    class Gender(models.TextChoices):
+        MALE = "M", "male"
+        FEMALE = "F", "female"
+        OTHER = "O", "other"
+
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    gender = models.Choices(Gender)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
